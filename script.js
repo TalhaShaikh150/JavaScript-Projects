381d9d6359cc9a74df3f3c3d49e1a87910e9f3de
TalhaShaikh150 <tssmart233@gmail.com>
2025-07-16 22:00:23 +0500

    Fixed Final Errors

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 let container = document.querySelector(".all-projects");
-let container = document.querySelector(".all-projects");
 
 let data = [
   {
     id: 1,
-    image: "/assets/scramble.png",
-    link: "https://talhashaikh150.github.io/scramble-js/",
-    name: "Word Scramble Game",
-    description: "A fun WordScramble game using HTML, CSS, and JavaScript. 🚀",
-    github: "https://github.com/TalhaShaikh150/scramble-js",
-    id: 1,
-    image: "assets/code-studio.png",
+    image: "/assets/code-studio.png",
     link: "https://talhashaikh150.github.io/Code-Editor/",
     name: "Code Studio Editor",
     description:
@@ -19,12 +12,7 @@ let data = [
   },
   {
     id: 2,
-    image: "/assets/todo.png",
-    link: "https://talhashaikh150.github.io/Todo/",
-    name: "Todo List",
-    description: "A to-do list with local storage and dark mode. ✅🌙 ",
-    github: "https://github.com/TalhaShaikh150/Todo",
-    image: "assets/weather.png",
+    image: "/assets/weather.png",
     link: "https://talhashaikh150.github.io/Weather-app/",
     name: "Weather App",
     description:
@@ -33,12 +21,7 @@ let data = [
   },
   {
     id: 3,
-    image: "/assets/notes.png",
-    link: "https://talhashaikh150.github.io/Notes-App/",
-    name: "Notes App",
-    description: "Notes App with CRUD and Local Storage support",
-    github: "https://github.com/TalhaShaikh150/Notes-App",
-    image: "assets/instagram.png",
+    image: "/assets/instagram.png",
     link: "https://talhashaikh150.github.io/Instagram-Clone/",
     name: "Instagram Clone",
     description:
@@ -47,13 +30,7 @@ let data = [
   },
   {
     id: 4,
-    image: "/assets/palindrome.png",
-    link: "https://talhashaikh150.github.io/palindrome-checker/",
-    name: "Palindrom Checker",
-    description:
-      "A simple palindrome checker using HTML, CSS, and JavaScript. 🔄✅",
-    github: "https://github.com/TalhaShaikh150/palindrome-checker",
-    image: "assets/scramble.png",
+    image: "/assets/scramble.png",
     link: "https://talhashaikh150.github.io/scramble-js/",
     name: "Word Scramble Game",
     description: "A fun WordScramble game using HTML, CSS, and JavaScript. 🚀",
@@ -61,13 +38,7 @@ let data = [
   },
   {
     id: 5,
-    image: "/assets/pixelart.png",
-    link: "https://talhashaikh150.github.io/Js-pixel-art/",
-    name: "Pixel Art Editor",
-    description:
-      "A simple pixel art editor made with HTML, CSS, and JavaScript. 🎨🖌️",
-    github: "https://github.com/TalhaShaikh150/Js-pixel-art",
-    image: "assets/todo.png",
+    image: "/assets/todo.png",
     link: "https://talhashaikh150.github.io/Todo/",
     name: "Todo List",
     description: "A to-do list with local storage and dark mode. ✅🌙 ",
@@ -75,12 +46,7 @@ let data = [
   },
   {
     id: 6,
-    image: "/assets/dicegame.png",
-    link: "https://talhashaikh150.github.io/Dice-game/",
-    name: "Dice Game",
-    description: "A simple dice game made with HTML, CSS, and JavaScript. 🎲✨",
-    github: "https://github.com/TalhaShaikh150/Dice-game",
-    image: "assets/notes.png",
+    image: "/assets/notes.png",
     link: "https://talhashaikh150.github.io/Notes-App/",
     name: "Notes App",
     description: "Notes App with CRUD and Local Storage support",
@@ -88,12 +54,32 @@ let data = [
   },
   {
     id: 7,
-    image: "assets/palindrome.png",
+    image: "/assets/palindrome.png",
     link: "https://talhashaikh150.github.io/palindrome-checker/",
     name: "Palindrom Checker",
     description:
       "A simple palindrome checker using HTML, CSS, and JavaScript. 🔄✅",
     github: "https://github.com/TalhaShaikh150/palindrome-checker",
+  },
+  {
+    id: 8,
+    image: "/assets/pixelart.png",
+    link: "https://talhashaikh150.github.io/Js-pixel-art/",
+    name: "Pixel Art Editor",
+    description:
+      "A simple pixel art editor made with HTML, CSS, and JavaScript. 🎨🖌️",
+    github: "https://github.com/TalhaShaikh150/Js-pixel-art",
+  },
+  {
+    id: 9,
+    image: "/assets/dicegame.png",
+    link: "https://talhashaikh150.github.io/Dice-game/",
+    name: "Dice Game",
+    description: "A simple dice game made with HTML, CSS, and JavaScript. 🎲✨",
+    github: "https://github.com/TalhaShaikh150/Dice-game",
+  },
+  {
+    id: 10,
     image: "/assets/recipe.png",
     link: "https://talhashaikh150.github.io/recipe-website/",
     name: "Recipe's Website",
@@ -102,13 +88,7 @@ let data = [
     github: "https://github.com/TalhaShaikh150/recipe-website",
   },
   {
-    id: 8,
-    image: "assets/pixelart.png",
-    link: "https://talhashaikh150.github.io/Js-pixel-art/",
-    name: "Pixel Art Editor",
-    description:
-      "A simple pixel art editor made with HTML, CSS, and JavaScript. 🎨🖌️",
-    github: "https://github.com/TalhaShaikh150/Js-pixel-art",
+    id: 11,
     image: "/assets/csstools.png",
     link: "https://talhashaikh150.github.io/Css-Tools/",
     name: "Css Tools",
@@ -117,7 +97,7 @@ let data = [
     github: "https://github.com/TalhaShaikh150/Css-Tools",
   },
   {
-    id: 9,
+    id: 12,
     image: "/assets/timer.png",
     link: "https://talhashaikh150.github.io/js-timer/",
     name: "Stop Watch",
@@ -125,7 +105,7 @@ let data = [
     github: "https://github.com/TalhaShaikh150/js-timer",
   },
   {
-    id: 10,
+    id: 13,
     image: "/assets/E-commerce.png",
     link: "https://talhashaikh150.github.io/E-Commerce/",
     name: "E-Commerce Website",
@@ -133,49 +113,7 @@ let data = [
     github: "https://github.com/TalhaShaikh150/E-Commerce",
   },
 ];
-    image: "assets/dicegame.png",
-    link: "https://talhashaikh150.github.io/Dice-game/",
-    name: "Dice Game",
-    description: "A simple dice game made with HTML, CSS, and JavaScript. 🎲✨",
-    github: "https://github.com/TalhaShaikh150/Dice-game",
-  },
-  {
-    id: 10,
-    image: "assets/recipe.png",
-    link: "https://talhashaikh150.github.io/recipe-website/",
-    name: "Recipe's Website",
-    description:
-      "A simple recipe website made with HTML, CSS, and JavaScript. 🍽️📖",
-    github: "https://github.com/TalhaShaikh150/recipe-website",
-  },
-  {
-    id: 11,
-    image: "assets/csstools.png",
-    link: "https://talhashaikh150.github.io/Css-Tools/",
-    name: "Css Tools",
-    description:
-      "A simple CSS box shadow tool made with HTML, CSS, and JavaScript. 🎨🖌️",
-    github: "https://github.com/TalhaShaikh150/Css-Tools",
-  },
-  {
-    id: 12,
-    image: "assets/timer.png",
-    link: "https://talhashaikh150.github.io/js-timer/",
-    name: "Stop Watch",
-    description: "A simple timer app made with HTML, CSS, and JavaScript. ⏱️🧭",
-    github: "https://github.com/TalhaShaikh150/js-timer",
-  },
-  {
-    id: 13,
-    image: "assets/E-commerce.png",
-    link: "https://talhashaikh150.github.io/E-Commerce/",
-    name: "E-Commerce Website",
-    description: "A simple timer app made with HTML, CSS, and JavaScript. ⏱️🧭",
-    github: "https://github.com/TalhaShaikh150/E-Commerce",
-  },
-];
 
-for (let i = 0; i < data.length; i++) {
 for (let i = 0; i < data.length; i++) {
   container.innerHTML += `<div class="col-md-4">
           <div class="project-card">
@@ -200,6 +138,5 @@ for (let i = 0; i < data.length; i++) {
               </a>
             </div>
           </div>
-        </div>`;
         </div>`;
 }
